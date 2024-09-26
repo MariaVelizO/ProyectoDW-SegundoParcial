3f7f8f3be95009ffe8c8a2cd40e3eb3b5193aee4
kgomezt2@miumg.edu.gt <kgomezt2@miumg.edu.gt>
2024-09-25 22:06:00 -0600

    subiendo la funcion para listar todos los productos y buscar producto por ID Karla Sofia Gómez Tobar 9959-21-1896

--- ProductosTecnologicosNODEJS/controllers/productos.controllers.js
+++ ProductosTecnologicosNODEJS/controllers/productos.controllers.js
@@ -1,3 +1,27 @@
 const Productos = require('../models/productos.model')
 
 //exportación de los métodos
+//Karla
+exports.getAllProductos = async (req, res)=> {
+    const productos = await Producto.find();
+    res.status(200).json(productos);
+};
+
+//Karla
+exports.getProductoporID = async (req, res) => {
+    try {
+        const idproducto = req.params.id;  
+
+        const producto = await Producto.findById(idproducto);
+
+        if (!producto) {
+            return res.status(404).send({ mensaje: 'Producto no encontrado' });
+        }
+
+        res.status(200).json(producto);
+    } catch (error) {
+        console.error('Error al obtener el producto:', error);
+        res.status(500).send({ mensaje: 'Error al obtener el producto' });  
+    }
+};
+
--- ProductosTecnologicosNODEJS/routes/productos.routes.js
+++ ProductosTecnologicosNODEJS/routes/productos.routes.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const {getAllProductos, getProductoporID} = require('../controllers/productos.controllers')
 
 //definir las funciones
+
+//listar de producto Karla
+router.get('/producto', getAllProductos);
+//listar por id especifico de producto Karla
+router.get('/producto/:id', getProductoporID);
+
 
 module.exports = router;
